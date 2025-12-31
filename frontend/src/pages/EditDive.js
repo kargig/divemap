b795a122d5f8a3a4051be24178f6042d927c40ec
Johnbin89 <john_biniaris@hotmail.com>
2025-12-31 12:30:51 +0200

    external links / photos /video only

--- frontend/src/pages/EditDive.js
+++ frontend/src/pages/EditDive.js
@@ -945,9 +945,8 @@ const EditDive = () => {
                             onChange={e => setNewMediaType(e.target.value)}
                             className='w-full border border-gray-300 rounded-md px-3 py-2'
                           >
-                            <option value='external_link'>External Link</option>
+                            <option value='photo'>Photo (URL)</option>
                             <option value='video'>Video</option>
-                            <option value='dive_plan'>Dive Plan</option>
                           </select>
                         </div>
 
